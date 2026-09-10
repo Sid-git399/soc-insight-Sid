2cf8f3127eaaf100b4b86b366f2690c29586b44c
Sid-git399 <ns_bedrane@esi.dz>
2026-09-10 02:34:54 +0100

    feat(correlation): initialize correlation package

---
+++ src/soc_insight/correlation/__init__.py
@@ -0,0 +1,3 @@
+from .correlator import build_entity_windows, entity_key, EntityWindow, DEFAULT_WINDOW_MINUTES
+
+__all__ = ["build_entity_windows", "entity_key", "EntityWindow", "DEFAULT_WINDOW_MINUTES"]
